3fdf3eaff72c3b81e380f67b00234fbd907bd340
jlazaro <jlazaro135@gmail.com>
2023-03-04 19:27:14 +0100

    se comienza proyecto cartelify

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import VueResizable from 'vue-resizable'
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="canvas">
+      <vue-resizable
+      :fit-parent="true"
+      >
+        <div class="resizable-content"></div>
+      </vue-resizable>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container{
+  min-height: 100vh;
+}
+
+.canvas{
+  aspect-ratio: 1/1.414;
+    background-color: red;
+    max-width: 50%;
+    height: 100%;
+    width: 100%;
+}
+.resizable-content {
+  height: 100%;
+  width: 100%;
+  background-color: aqua;
+}
+</style>
